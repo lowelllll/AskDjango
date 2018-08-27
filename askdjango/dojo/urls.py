3a59fad0131319a099ae06be5fb7700b55232a36
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-08-27 22:52:25 +0900

    [Django 기본] URLConf 정규식 연습

--- askdjango/dojo/urls.py
+++ askdjango/dojo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$',views.mysum),
+    url(r'^sum/(?P<numbers>[\d/]+)/$',views.mysum), # sum/1/2/3/4/5.../10 여러개 변수 등록
+    url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/',views.hello),
 ]
--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -3,5 +3,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def mysum(request,x,y):
-    return HttpResponse(int(x)+int(y))
+def mysum(request,numbers):
+    # numbers = "1/2/3/4/5.../10"
+    # result = sum(map(int,numbers.splite("/"))
+    result = sum(map(lambda s:int(s or 0),numbers.split("/"))) # s가 int 타입이 아닐 경우 false -> 0으로 치환 (빈문자 치환)
+    return HttpResponse(result)
+
+
+def hello(request,name,age):
+    return HttpResponse("안녕하세요.{}.{}살이시네요.".format(name,age))
