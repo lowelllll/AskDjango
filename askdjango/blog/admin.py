00a03a53d3a0d44d5a517cdb1e9ba7f7ea2b33c8
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-08-30 00:40:48 +0900

    [Django 기본]blog.model.Post 모델의 PostAdmin 커스텀

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -3,4 +3,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post) # admin 사이트 등록
+# admin.site.register(Post) admin 사이트 등록
+
+@admin.register(Post) # 데코레이터로 등록.
+class PostAdmin(admin.ModelAdmin): # Custom Admin
+    list_display = ['id','title','created_at']
+
+# admin.site.register(Post,PostAdmin) register 함수로 등록.
