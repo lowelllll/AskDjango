72a84775a7f16c3edd2e4ca04eaac0ff7f6e9f60
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-09-01 16:05:29 +0900

    [Django 기본]EP10_http-status-code
    get_object_or_404
    raise Http404

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$',views.post_list)
+    url(r'^$',views.post_list),
+    url(r'^(?P<id>\d+)/$',views.post_detail),
 ]
--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,9 +1,9 @@
 # blog/views.py
-
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
-def post_list(request): # render
+def post_list(request): # Post 리스트 보기 구현.
     qs = Post.objects.all()
 
     q = request.GET.get('q','')
@@ -14,4 +14,16 @@ def post_list(request): # render
     return render(request,'blog/post_list.html',{
         'post_list':qs,
         'q':q
+    })
+
+def post_detail(request, id): # Post 상세보기 구현
+    # try:
+    #     post = Post.objects.get(id=id) 해당 id의 포스트가 없을 시
+    # except:
+    #     raise Http404  404 error 발생
+    
+    post = get_object_or_404(Post,id=id) # 위의 코드와 동일 추천! 
+
+    return render(request, 'blog/post_detail.html', {
+        'post':post
     })
