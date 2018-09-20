2bdf05c2ffe1c88eb46415ffb5a0f480883fed3f
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-09-21 00:04:23 +0900

    [django API]SearchFilter를 통한 필터링

--- apiprj/ep04/admin.py
+++ apiprj/ep04/admin.py
@@ -5,3 +5,4 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','content','is_public']
+    search_fields = ('title',) # 검색
--- apiprj/ep06/views.py
+++ apiprj/ep06/views.py
@@ -1,15 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from ep04.models import Post
 from ep04.serializer import PostSerializer
+
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']  # title 필드에서 검색 수행  ep06/post/?search=쿡
 
-    def get_queryset(self): # 필터링
+    def get_queryset(self): # 쿼리셋 필터링
         qs = super().get_queryset()
         if self.request.user.is_authenticated:
             qs = qs.filter(user=self.request.user) # 현재 로그인 되어있는 유저와 동일한지
         else:
             qs = qs.none() # empty result
         return qs
+
