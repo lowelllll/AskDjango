6d437b9758b8f103f8fd37c6eb4e70a49821671f
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-09-21 22:59:22 +0900

    diary -> kakaoprj

--- diary/plusfriend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlusfriendConfig(AppConfig):
-    name = 'plusfriend'
--- diary/plusfriend/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    message = models.TextField(blank=True)
-    photo = models.ImageField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
