8cf608c88304b36dee14c5c6e46d19c48b948957
mac <lowell@macui-Mac-mini.local>
2018-09-21 11:47:06 +0900

    [django API] Token
    Authentication 방법을 Token으로 설정!
    Token은 user모델과 1:1 관계를 맺으며
    api-token-auth 라는 url로 호출 시(POST,user info), Token 자동생성 and 반환!

--- apiprj/apiprj/urls.py
+++ apiprj/apiprj/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/',include('rest_framework.urls',namespace='rest-framework')), # API 페이지에서 로그인기능 추가
+    url(r'^api-token-auth/$',obtain_auth_token), # /api-token-auth POST 요청으로 아이디와 비밀번호를 같이 보내면 해당 유저의 토큰을 반환.
+
     url(r'^sample/',include('sample.urls',namespace='sample')),
     url(r'^ep03/',include('ep03.urls',namespace='ep03')),
     url(r'^ep04/',include('ep04.urls',namespace='ep04')),
     url(r'^ep06/',include('ep06.urls',namespace='ep06')),
     url(r'^ep08/',include('ep08.urls',namespace='ep08')),
+    url(r'^api/', include('api.urls', namespace='api')),
+
 ]
